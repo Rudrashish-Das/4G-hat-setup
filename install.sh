828a70f63b209376bd54b822d17050246e3fbde9
Rudrashish Das <rudrashishdas0@gmail.com>
2023-02-07 12:12:17 +0530

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 mkdir -p /home/$(whoami)/igdrones/startup/
 cp ./scripts/ssh-tunnel-ngrok.sh -t /home/$(whoami)/igdrones/startup/
 sudo cp ./scripts/ssh-tunnel-ngrok.service -t /etc/systemd/system/
+sudo chmod +x /home/$(whoami)/igdrones/startup/ssh-tunnel-ngrok.sh
 sudo systemctl daemon-reload
 sudo systemctl enable ssh-tunnel-ngrok.service
 sudo systemctl --all | ssh-tunnel-ngrok.service
