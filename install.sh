d14e94a3680ed9add5080b22c516287a8a731135
Rudrashish Das <rudrashishdas0@gmail.com>
2023-02-07 13:14:18 +0530

    Bug fixes

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
 mkdir -p /home/$(whoami)/igdrones/startup/
 cp ./scripts/ssh-tunnel-ngrok.sh -t /home/$(whoami)/igdrones/startup/
 sudo cp ./scripts/ssh-tunnel-ngrok.service -t /etc/systemd/system/
 sudo chmod +x /home/$(whoami)/igdrones/startup/ssh-tunnel-ngrok.sh
 sudo systemctl daemon-reload
 sudo systemctl enable ssh-tunnel-ngrok.service
-sudo systemctl --all | ssh-tunnel-ngrok.service
+sudo systemctl --all | grep ssh-tunnel-ngrok.service
 sudo systemctl start ssh-tunnel-ngrok.service
+
--- scripts/ssh-tunnel-ngrok.sh
+++ scripts/ssh-tunnel-ngrok.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 while [ 1 == 1 ]; do
 	ngrok tcp 22 --authtoken "2L6MgHoQRGGri7Um0pP6ohg3k5A_5yGFgi3tzwgh39zK1NnRs"
 done
